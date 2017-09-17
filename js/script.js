84ae559f8703085c911b8a303bb5ed1485eddf3d
dominicdaviesme <domwales@outlook.com>
2017-09-17 20:30:55 +0100

    Added scroll funtionality to hero buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	
+	
+	//	Sticky Navigation
 	$('.js--section-features').waypoint(function(direction) {
 		if (direction == "down") {
 			$('nav').addClass('sticky');
@@ -9,4 +11,22 @@ $(document).ready(function() {
 	}, {
   	offset: '60px;'																			 
 	});	
+	
+	
+	
+	//	Scroll on Buttons
+	
+	//	I'm Hungry Button
+	$('.js--scroll-to-plans').click(function () {
+		$('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+	});
+	
+	
+	// Show me more Button
+	$('.js--scroll-to-start').click(function () {
+		$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+	});
+	
+	
+	
 });
